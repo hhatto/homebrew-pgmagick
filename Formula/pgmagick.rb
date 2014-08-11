44a5b8157d47a42b4a67b81a627fc1119e63e8c8
Hideo Hattori <hattori-h@klab.com>
2014-08-11 14:58:05 +0900

    fix sha1

--- Formula/pgmagick.rb
+++ Formula/pgmagick.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Pgmagick < Formula
   homepage 'https://pypi.python.org/pypi/pgmagick/'
   url 'https://bitbucket.org/hhatto/pgmagick/get/ver0.5.8.tar.bz2'
-  sha1 'e99bf5f7b75a8ec903d5dace86b5b91fc2bcca06'
+  sha1 'cbb9d00fb1620659f7e99ea2f94282de71470f26'
 
   depends_on 'python'
   depends_on 'boost' => [:optional, 'with-thread-unsafe', 'build-from-source', '--with-python']
