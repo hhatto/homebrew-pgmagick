ce889b948dae7b7a1f92b69c925a35aea888c01f
Hideo Hattori <hattori-h@klab.com>
2014-08-11 14:03:34 +0900

    update version0.5.8

--- Formula/pgmagick.rb
+++ Formula/pgmagick.rb
@@ -2,11 +2,11 @@ require 'formula'
 
 class Pgmagick < Formula
   homepage 'https://pypi.python.org/pypi/pgmagick/'
-  url 'https://bitbucket.org/hhatto/pgmagick/get/ver0.5.7.tar.bz2'
-  sha1 '62d00f1d66a6a357327cdd71000fa2d1e93629f7'
+  url 'https://bitbucket.org/hhatto/pgmagick/get/ver0.5.8.tar.bz2'
+  sha1 'e99bf5f7b75a8ec903d5dace86b5b91fc2bcca06'
 
   depends_on 'python'
-  depends_on 'boost' => [:optional, 'with-thread-unsafe', 'build-from-source']
+  depends_on 'boost' => [:optional, 'with-thread-unsafe', 'build-from-source', '--with-python']
   depends_on 'graphicsmagick' => 'with-magick-plus-plus'
 
   def install
