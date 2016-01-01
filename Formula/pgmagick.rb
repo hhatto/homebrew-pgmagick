98ed47239b6d529c04b818e457c2b819d7b63e6c
hhatto <hhatto.jp@gmail.com>
2016-01-01 23:11:54 +0900

    pgmagick ver0.6

--- Formula/pgmagick.rb
+++ Formula/pgmagick.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Pgmagick < Formula
   homepage 'https://pypi.python.org/pypi/pgmagick/'
-  url 'https://bitbucket.org/hhatto/pgmagick/get/ver0.5.12.tar.bz2'
-  sha1 'ba75b74eb17e8756c2ff2406e743e7400c1d6ab7'
+  url 'https://github.com/hhatto/pgmagick/archive/ver0.6.tar.gz'
+  sha1 'bdddb477867d94c64686130f9dc9d15f5408ec29'
 
   depends_on 'python'
   depends_on 'boost' => ['with-thread-unsafe', 'build-from-source']
