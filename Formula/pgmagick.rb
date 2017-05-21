7035ea4ae6f34747240d978a18c7e179e11623f2
Hideo Hattori <hattori-h@klab.com>
2017-05-21 10:55:30 +0900

    fix install boost-python

--- Formula/pgmagick.rb
+++ Formula/pgmagick.rb
@@ -1,9 +1,8 @@
-require 'formula'
-
 class Pgmagick < Formula
-  homepage 'https://pypi.python.org/pypi/pgmagick/'
-  url 'https://github.com/hhatto/pgmagick/archive/ver0.6.5.tar.gz'
-  sha256 '33c19044c24c100d3d0df2e9d8d2254955e9869e23740e004800655c1826839d'
+  desc "yet another boost-python based wrapper for GraphicsMagick"
+  homepage "https://pypi.python.org/pypi/pgmagick/"
+  url "https://github.com/hhatto/pgmagick/archive/ver0.6.5.tar.gz"
+  sha256 "33c19044c24c100d3d0df2e9d8d2254955e9869e23740e004800655c1826839d"
 
   option "with-python3", "support python3"
   depends_on "python3" => :optional
@@ -12,9 +11,13 @@ class Pgmagick < Formula
   # option "with-imagemagick", "Build with ImageMagick"
 
   depends_on :python if MacOS.version <= :snow_leopard
-  depends_on 'graphicsmagick'
-  depends_on 'boost' => ['with-thread-unsafe', 'build-from-source']
-  depends_on 'boost-python' => [:optional, 'with-python3']
+  depends_on "graphicsmagick"
+  # depends_on 'boost' => ['with-thread-unsafe', 'build-from-source']
+  if build.with? "python3"
+    depends_on "boost-python" => ["with-python3"]
+  else
+    depends_on "boost-python"
+  end
 
   ## not support yet
   # if build.with? "imagemagick"
@@ -27,10 +30,10 @@ class Pgmagick < Formula
   def install
     if build.with? "python3"
       ENV.prepend_create_path "PYTHONPATH", "#{lib}/python3.6/site-packages"
-      system 'python3', 'setup.py', 'install', "--prefix=#{prefix}"
+      system "python3", "setup.py", "install", "--prefix=#{prefix}"
     else
       ENV.prepend_create_path "PYTHONPATH", "#{lib}/python2.7/site-packages"
-      system 'python', 'setup.py', 'install', "--prefix=#{prefix}"
+      system "python", "setup.py", "install", "--prefix=#{prefix}"
     end
   end
 end
