eb384f93a9854755a28ef2a50ecf2b9f1e863950
Hideo Hattori <hhatto.jp@gmail.com>
2018-10-13 00:14:52 +0900

    pgmagick v0.7.4 and recent homebrew's package

--- Formula/pgmagick.rb
+++ Formula/pgmagick.rb
@@ -1,20 +1,19 @@
 class Pgmagick < Formula
   desc "yet another boost-python based wrapper for GraphicsMagick"
   homepage "https://pypi.python.org/pypi/pgmagick/"
-  url "https://github.com/hhatto/pgmagick/archive/ver0.6.5.tar.gz"
-  sha256 "33c19044c24c100d3d0df2e9d8d2254955e9869e23740e004800655c1826839d"
+  url "https://github.com/hhatto/pgmagick/archive/ver0.7.4.tar.gz"
+  sha256 "a3cb2d5fec6bd8aa63fb0fcfd819ae97c602582df94ff62b0bca0360a75758f2"
 
-  option "with-python3", "support python3"
-  depends_on "python3" => :optional
+  option "with-python2", "use python2"
+  depends_on "python@2" => :optional
 
   ## not support yet
   # option "with-imagemagick", "Build with ImageMagick"
 
   depends_on :python if MacOS.version <= :snow_leopard
   depends_on "graphicsmagick"
-  # depends_on 'boost' => ['with-thread-unsafe', 'build-from-source']
-  if build.with? "python3"
-    depends_on "boost-python" => ["with-python3"]
+  if build.with? "python"
+    depends_on "boost-python3"
   else
     depends_on "boost-python"
   end
@@ -28,12 +27,11 @@ class Pgmagick < Formula
   # end
 
   def install
-    if build.with? "python3"
-      ENV.prepend_create_path "PYTHONPATH", "#{lib}/python3.6/site-packages"
-      system "python3", "setup.py", "install", "--prefix=#{prefix}"
+    if build.with? "python"
+      ENV.prepend_create_path "PYTHONPATH", "#{lib}/python3.7/site-packages"
     else
       ENV.prepend_create_path "PYTHONPATH", "#{lib}/python2.7/site-packages"
-      system "python", "setup.py", "install", "--prefix=#{prefix}"
     end
+    system "python", "setup.py", "install", "--prefix=#{prefix}"
   end
 end
